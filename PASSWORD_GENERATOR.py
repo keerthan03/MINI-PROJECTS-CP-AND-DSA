981e62a1bda33b9ae01d983f2e5de5beffe2f9cf
keerthan03 <137698107+keerthan03@users.noreply.github.com>
2023-08-05 17:42:12 +0530

    Add files via upload

---
+++ PASSWORD_GENERATOR.py
@@ -0,0 +1,27 @@
+#importing string and random library
+import string
+import random
+
+#input from the user
+a = int(input("How many letters do you want in your password? "))
+b = int(input("How many numbers do you want in your password? "))
+c = int(input("How many symbols do you want in your password? "))
+
+#using some predefined fuction from the respective library
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+numbers = string.digits
+symbols = string.punctuation
+
+# using random.choices() to allow repetition
+letter = random.choices(upper + lower,k=a)
+num = random.choices(numbers, k=b)
+sym = random.choices(symbols, k=c)
+
+# concatenate the lists and shuffle them
+word = letter + num + sym
+random.shuffle(word)
+
+# join the shuffled list elements to create the password
+password = "".join(word)
+print(password)
